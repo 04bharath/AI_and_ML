e65c56480857b6c89d1bf6542cedce40eef44119
bharath <bharath1232@gmail.com>
2023-08-17 12:31:43 +0530

    change1

---
+++ mathfunction.py
@@ -0,0 +1,13 @@
+from numpy import * 
+a = array([10, 2, 3, 40]) 
+b = array([1, 2, 3, 4]) 
+arr1 = a - b
+print("sub of array = ",arr1)
+arr2 = a+b
+print("add of array = ",arr2)
+arr3 = a*b
+print("Multiply of array = ",arr3)
+arr4 = a**b
+print("Squre of array = ",arr4)
+arr5 = a/b
+print("Divided of array = ",arr5)
