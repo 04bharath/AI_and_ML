98eab0ce1e7b4bd9deb69a97198cac06ab58a8ff
bharath <bharath123@gmail.com>
2023-08-18 10:54:57 +0530

    change2

--- lambadfunctions.py
+++ lambadfunctions.py
@@ -26,4 +26,18 @@ lst5 = [5,6,8,4,2]
 x1 = list(map(lambda x: x*x,lst5))
 print("squre of each number =",x1)
 
+l1 = [1,2,3,4,5]
+l2 = [6,7,8,9,10]
+l3 = [8,6,5,4,1]
+l = list(map(lambda x,y,z:x+y+z,l1,l2,l3))
+print("Add the 3 list=",l)
+
+fruits = ['mango','orange','apple','kiwi']
+l1 = list(filter(lambda x:'g' in x,fruits))
+print("G in the list = ",l1)
+
+fruits = ['mango','orange','apple','kiwi']
+l2 = list(filter(lambda x:'g' not in x,fruits))
+print("G not in list = ",l2)
+
 
